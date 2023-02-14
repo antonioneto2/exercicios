0dc83e80a7ddf856129eb65b51e33486b696757e
Antonio Neto <113268997+antonioneto2@users.noreply.github.com>
2023-02-14 10:27:20 -0300

    new lines

--- exercícios C/Q4.c
+++ exercícios C/Q4.c
@@ -1,4 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct
+typedef struct aluno
+{
+    char nome[50];
+    int matricula;
+} Aluno;
+
+int main()
+{
+    int qtd_alunos, index;
+    printf("Qauntos alunos?");
+    scanf("%d", &qtd_alunos);
+    Aluno **vetor = (Aluno **)malloc(qtd_alunos * sizeof(Aluno *));
+    if (vetor == NULL)
+    {
+        printf("ERRO!");
+        exit(1);
+    }
+
+    for (index = 0; index < qtd_alunos; index++)
+    {
+        printf("\nDigite o nome: ");
+        scanf(" %[^\n]s", vetor[index]->nome);
+        printf("Digite a matricula: ");
+        scanf("%d", &vetor[index]->matricula);
+        printf("\n");
+        printf("|Dados| \nNome: %s \nMatricula: %d\n", vetor[index]->nome, vetor[index]->matricula);
+    }
+    for (index = 0; index < qtd_alunos; index++)
+    {
+        free(vetor);
+    }
+    return 0;
+}
